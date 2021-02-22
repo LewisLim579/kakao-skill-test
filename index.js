94c59c46b871ca31c99a9bacd26b4b86740eee3c
LewisLim579 <samo57980@gmail.com>
2021-02-22 16:05:41 +0900

    s

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ app.use(logger('dev', {}));
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
+apiRouter.get('/', function(req, res) {
+    const responseBody = {
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            simpleText: {
+              text: "기본index포트 "
+            }
+          }
+        ]
+      }
+    };
+  
+    res.status(200).send(responseBody);
+  });
+
 apiRouter.get('/sayHello', function(req, res) {
     const responseBody = {
       version: "2.0",
@@ -26,42 +43,7 @@ apiRouter.get('/sayHello', function(req, res) {
     res.status(200).send(responseBody);
   });
 
-apiRouter.post('/sayHello', function(req, res) {
-    const responseBody = {
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            simpleText: {
-              text: "hello I'm Ryan"
-            }
-          }
-        ]
-      }
-    };
-  
-    res.status(200).send(responseBody);
-  });
-  
-  apiRouter.post('/showHello', function(req, res) {
-    console.log(req.body);
-  
-    const responseBody = {
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            simpleImage: {
-              imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
-              altText: "hello I'm Ryan"
-            }
-          }
-        ]
-      }
-    };
-  
-    res.status(200).send(responseBody);
-  });
+
 
   app.listen(3000, function() {
     console.log('Example skill server listening on port 3000!');
