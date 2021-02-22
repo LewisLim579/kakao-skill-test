ade79116bc323b87750d1ca5d560d96d61fad645
LewisLim579 <samo57980@gmail.com>
2021-02-22 17:27:45 +0900

    s

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ apiRouter.get('/sayHello', function(req, res) {
 
   apiRouter.post('/sayHello', function(req, res) {
 
-    console.log("이 로그로 파라메터 받아오나2222? "+ req.body.phonenum);
+    console.log("이 로그로 파라메터 받아오나2222? "+ req.params.phonenum);
     const responseBody = {
       version: "2.0",
       data: {
